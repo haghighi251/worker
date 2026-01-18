b848e0404d9e933eba08716578a9ef26453b8ee2
Amir <haghighi251@gmail.com>
2026-01-18 15:42:46 +0100

    feat: add creadit check middleware

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import companyRouter  from "@/infrastructure/http/routes/company";
 import { getAppDataSource } from '@/infrastructure/database/app-data-source';
 import { setupLogging } from '@/infrastructure/logs/logging';
 import { ROUTES } from '@/infrastructure/http/routes/routes';
-import { setupProxies } from '@/infrastructure/shared/proxy';
-import { setupRateLimit } from '@/infrastructure/shared/rate-limit';
+import { setupProxies } from '@/infrastructure/shared/middlewares/proxy';
+import { setupRateLimit } from '@/infrastructure/shared/middlewares/rate-limit';
+import { setupCreditCheck } from '@/infrastructure/shared/middlewares/creadit-check';
 
 const app = express();
 dotenv.config();
@@ -15,6 +16,7 @@ dotenv.config();
 // To log the incoming requests
 setupLogging(app);
 setupRateLimit(app, ROUTES);
+setupCreditCheck(app, ROUTES);
 setupProxies(app, ROUTES);
 
 const port = process.env.PORT || 8000;
